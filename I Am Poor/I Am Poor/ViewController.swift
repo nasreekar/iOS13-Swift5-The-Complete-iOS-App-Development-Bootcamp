e26203608b7af148773e94ccbe1d4305f75c5f46
Abhijith Sreekar <abhijith.sreekar@infra.tigerspike.com>
2020-06-02 00:54:32 +0530

    Initial Commit to Parent folder with first two projects

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Abhijith Sreekar on 2/6/20.
+//  Copyright © 2020 Abhijith Sreekar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Abhijith Sreekar on 1/6/20.
+//  Copyright © 2020 Abhijith Sreekar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
