ce44a52eb741d3249282fec3fb8931c602db3cb4
Abhijith Sreekar <nasreekar@gmail.com>
2020-06-02 13:43:18 +0530

    Randomise the dice on button click

--- Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // IBOutlet allows me to reference an UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    // as the dice array is not changing anywhere in the code,
+    // it is set as 'let' constant instead of 'var' constant
+    let diceArray = [ #imageLiteral(resourceName: "DiceOne") , #imageLiteral(resourceName: "DiceTwo") , #imageLiteral(resourceName: "DiceThree") , #imageLiteral(resourceName: "DiceFour") , #imageLiteral(resourceName: "DiceFive") , #imageLiteral(resourceName: "DiceSix")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func rollBtnPressed(_ sender: UIButton) {
+        // while Int.random(in: 0...5) gives a random value
+        // It is used for other big purposes
+        // For this use, we can use randomElement property of array
+        // to acheive the same functionality
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
     }
 
-
 }
+
 
